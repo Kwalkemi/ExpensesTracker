7e52d91e0d4a5ba6754263d7f6159f76d4de5df5
Bharat Talreja <bharattalreja50@gmail.com>
2020-07-24 02:02:15 +0530

    [Expenses]:- Added Husband Wife Form.

--- ExpensesTracker/Properties/Settings.Designer.cs
+++ ExpensesTracker/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace ExpensesTracker.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.5.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.6.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -20,6 +20,17 @@ namespace ExpensesTracker.Properties {
         public static Settings Default {
             get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=LAPTOP-86PJE7FL\\SQLEXPRESS;Initial Catalog=Expenses_Tracker;Integrate" +
+            "d Security=True")]
+        public string Expenses_TrackerConnectionString {
+            get {
+                return ((string)(this["Expenses_TrackerConnectionString"]));
             }
         }
     }
